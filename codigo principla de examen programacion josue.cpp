2b0213392e247c6a46512e4e7376c44dd7a40287
Josue Enamorado <enamoradojr11@gmail.com>
2022-03-06 17:48:09 -0600

    cuarta funcion calculo de numeros pares e impares

--- codigo principla de examen programacion josue.cpp
+++ codigo principla de examen programacion josue.cpp
@@ -86,3 +86,29 @@ if(op==2){
 		  	
 		  	
 		  }
+		  if(op==3){
+            	 int numero, pares, impares;
+
+    printf( "\n   Introduzca un n%cmero entero (0=Fin): ", 163 );
+    scanf( "%d", &numero );
+
+    pares = 0;
+    impares = 0;
+
+    while ( numero != 0 )
+    {
+        if ( numero % 2 == 0 )
+            pares += numero;
+        else
+            impares += numero;
+
+        printf( "\n   Introduzca un n%cmero entero (0=Fin): ", 163 );
+        scanf( "%d", &numero );
+    } 
+
+    printf( "\n   La suma de los pares es: %d", pares );
+    printf( "\n\n   La suma de los impares es: %d", impares );
+
+    getch(); /* Pausa */
+		  }
+		  
