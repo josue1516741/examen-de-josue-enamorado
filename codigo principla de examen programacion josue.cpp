02ddcbd1e535d3690f4e5ded41ec22adc2af0844
Josue Enamorado <enamoradojr11@gmail.com>
2022-03-06 17:44:14 -0600

    tercera funcion tablas de multiplicar

--- codigo principla de examen programacion josue.cpp
+++ codigo principla de examen programacion josue.cpp
@@ -59,3 +59,30 @@ system("color b0");
    cout << endl << "Factorial de " << n << " -> " << factorial << endl;
    system("pause");
 }
+if(op==2){
+		   char seguir;
+    int i, numero;
+
+    do
+    {
+        printf( "\n   Introduzca un n%cmero entero: ", 163 );
+        scanf( "%d", &numero );
+
+        printf( "\n   La tabla de multiplicar del %d es:\n", numero );
+
+        /* Inicio del anidamiento */
+
+        for ( i = 1 ; i <= 10 ; i++ )
+            printf( "\n   %d * %d = %d", i, numero, i * numero );
+
+        /* Fin del anidamiento */
+
+        printf( "\n\n   %cDesea ver otra tabla (s/n)?: ", 168 );
+        fflush( stdin );
+        scanf( "%c", &seguir );
+
+    } while ( seguir != 'n' );
+
+		  	
+		  	
+		  }
